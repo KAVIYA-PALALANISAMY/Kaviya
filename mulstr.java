246b4ff978dd16c5756195ec748b494fb2c9357b
KAVIYA-PALALANISAMY <merlinkaviya@gmail.com>
2017-06-15 22:17:00 +0530

    Create mulstr.java

--- mulstr.java
+++ mulstr.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+
+  import java.util.Scanner;
 class mulstr
 {
   public static void main(String args[])
@@ -6,11 +7,13 @@ class mulstr
      Scanner s=new Scanner(System.in);
      String s1=s.nextLine();
      String[] s2=s1.split("");
-     int n1=Integer.parseInt(s2[0]);
-     int n2=Integer.parseInt(s2[1]);
-      int n3=n1*n2;
-     System.out.print(Integer.toString(n3));
+     long n1=Long.parseLong(s2[0]);
+     long n2=Long.parseLong(s2[1]);
+   long n3=0;
+    if(n1 >0&& n2>0)
+     {
+      n3=n1*n2;
+      }
+     System.out.print(Long.toString(n3));
    }
 }
-
-  
